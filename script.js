67f916c998ea23221f1370771d086c10241c4946
Perry Theobald <perrytjr@gmail.com>
2020-07-25 10:25:18 -0700

    finally have a timer on page used from class activity

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+var timeEl = document.querySelector(".time");
 
+var secondsLeft = 10;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " Time left for quiz.";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+setTime();
