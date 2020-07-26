b2c2e7198afb325f0b500eab6e62ba2795e85ccc
Perry Theobald <perrytjr@gmail.com>
2020-07-26 12:43:28 -0700

    questions and answers from gif file are in jspage still have to set up function and what not

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+//timer selector / timer variable seconds left and timer function still needs to be set up 
 var timeEl = document.querySelector(".time");
-//clear button for highscores page 
-var clearEl = document.querySelector("#clearbutton");
+
 var secondsLeft = 75;
 
 function setTime() {
@@ -17,12 +17,47 @@ function setTime() {
 }
 setTime();
 
+// questions from the GIF that was sent to us and their answers 
+
+/*Commonly used data types do not include:
+  a. string
+  b. boolean
+  c. alerts
+  d. number
+  answer is c
+
+  The condition in an if else statement is enclosed within_____.
+  a. quotes
+  b. curly brackets
+  c. parenthesis
+  d. square brackets
+  answer is c
+
+   Arrays in javascript can be used to store ____.
+   a. numbers and strings
+   b. other arrays
+   c. booleans
+   d. all of above 
+   answer is d 
+
+   String values must be enclosed within ____ when being assigned to variables.
+   a. commas
+   b. curly brakets
+   c. quotes
+   d. parenthesis
+   answer is c
+
+  A very useful tool used during development and debugging for printing content to the debugger is:
+  a. javascript
+  b. terminal/bash
+  c. for loops
+  d. console.log 
+  answer is d */
 
 
 
-
-
-
+//clear button for highscores page 
+var clearEl = document.querySelector("#clearbutton");
 //clear button for highscores page still have to set up score form and connect button to clear scores
 clearEl.addEventListener("click", function (event) {
   event.preventDefault();
