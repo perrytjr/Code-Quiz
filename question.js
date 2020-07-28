d774b656182ef549100c262b7e812111d9054b29
Perry Theobald <perrytjr@gmail.com>
2020-07-28 13:40:09 -0700

    different approach last attempt totaly did not work

---
+++ question.js
@@ -0,0 +1,28 @@
+let questions = [
+    {
+      title: "Commonly used data types do not include?",
+      choices: ["string","boolean", "alerts",  "number"],
+      answer: "script"
+    },
+    {
+      title: "The condition in an if else statement is enclosed within_____.",
+      choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+      answer: "parenthesis"
+    },
+    {
+      title: "Arrays in javascript can be used to store ____.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of above"],
+      answer: "all of above"
+    },
+    {
+  
+      title: "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas","curly brakets", "quotes", "parenthesis"],
+      answer: "quotes"
+    },
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is?",
+      choices: ["javascript", "terminal/bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+  ];
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var clearEl = document.querySelector("#clearbutton");
 
 
 
-startbutton.addEventListener("click", setTime);
+//startbutton.addEventListener("click", setTime);
 
 
 function setTime() {
@@ -26,42 +26,12 @@ setTime();
 
 
 
-// questions from the GIF that was sent to us and their answers 
 
-/*Commonly used data types do not include:
-  a. string
-  b. boolean
-  c. alerts
-  d. number
-  answer is c
 
-  The condition in an if else statement is enclosed within_____.
-  a. quotes
-  b. curly brackets
-  c. parenthesis
-  d. square brackets
-  answer is c
 
-   Arrays in javascript can be used to store ____.
-   a. numbers and strings
-   b. other arrays
-   c. booleans
-   d. all of above 
-   answer is d 
 
-   String values must be enclosed within ____ when being assigned to variables.
-   a. commas
-   b. curly brakets
-   c. quotes
-   d. parenthesis
-   answer is c
 
-  A very useful tool used during development and debugging for printing content to the debugger is:
-  a. javascript
-  b. terminal/bash
-  c. for loops
-  d. console.log 
-  answer is d */
+
 
 
 
